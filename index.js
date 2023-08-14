7f9de1c1b1cf561cbfda1143381d98ab388efcb4
ralstonb9 <ralstonb9@gmail.com>
2023-08-13 17:52:16 -0700

    added the questions for generating the readme and the required packages

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "Title",
+    },
+    {
+        type: "input",
+        message: "Provide a description of your project and what it does.",
+        name: "Description",
+    },
+    {
+        type: "input",
+        message: "Provide the steps needed to install your project and get the development environment running.",
+        name: "Installation",
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for how to use your project.",
+        name: "Usage",
+    },
+    {
+        type: "input",
+        message: "List any collaborators or third-party assets with their name and a link to reach them.",
+        name: "Credits",
+    },
+    {
+        type: "input",
+        message: "What type of licensing does your project have?",
+        name: "License",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
